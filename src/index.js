df8d28e0d262cb5ca36cd296a326daaa0e3a639e
Stephen Brewer <stephebrewer16@gmail.com>
2020-08-14 18:48:42 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDk5TBEzWCi9F0Sh0-2eIqVksb3T_81g-Q",
+  authDomain: "brewer-weather-app.firebaseapp.com",
+  databaseURL: "https://brewer-weather-app.firebaseio.com",
+  storageBucket: "brewer-weather-app.appspot.com",
+}
+
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <React.StrictMode>
